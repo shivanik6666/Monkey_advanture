e71d58ecec725ce2589e473caa628489560ecd46
shivanik6666 <87293059+shivanik6666@users.noreply.github.com>
2023-10-31 11:14:23 +0530

    Add files via upload
    It's an basic design of monkey game.

---
+++ Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>dragon game</title>
+    <link rel="stylesheet" href="StyleSheet.css" />
+    <script src="JavaScript.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+     <div class="gameContainer">
+            <div class="gameOver">Game Over</div>
+            <div class="dragon dragonAni"></div>
+            <div id="scoreCont">Your Score: 0</div>
+
+            <div class="obstacles"></div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ JavaScript.js
@@ -0,0 +1,53 @@
+﻿var score = 0;
+var cross = true;
+
+
+document.onkeydown = function (e) {
+    var obstacles = document.querySelector('.obstacles');
+    var obstaclesX = parseInt(window.getComputedStyle(obstacles).getPropertyValue('left'));
+
+    if (e.keyCode === 38) {
+        obstacles.classList.add('animatedobstacles');
+        setTimeout(function () {
+            obstacles.classList.remove('animatedobstacles');
+        }, 700);
+    } else if (e.keyCode === 39) {
+        obstacles.style.left = (obstaclesX + 300) + "px";
+    } else if (e.keyCode === 37) {
+        obstacles.style.left = (obstaclesX - 112) + "px";
+    }
+};
+
+setInterval(function () {
+    var dragon = document.querySelector('.dragon');
+    var gameOver = document.querySelector('.gameOver');
+    var obstacles = document.querySelector('.obstacles');
+
+    var dx = parseInt(window.getComputedStyle(obstacles).getPropertyValue('left'));
+    var dy = parseInt(window.getComputedStyle(obstacles).getPropertyValue('top'));
+
+    var ox = parseInt(window.getComputedStyle(dragon).getPropertyValue('left'));
+    var oy = parseInt(window.getComputedStyle(dragon).getPropertyValue('top'));
+
+    var offsetX = Math.abs(dx - ox);
+    var offsetY = Math.abs(dy - oy);
+
+    if (offsetX < 145 && offsetY < 53) {
+        gameOver.style.visibility = 'visible';
+        dragon.classList.remove('dragonAni');
+    } else if (offsetX < 145 && cross) {
+        score += 100;
+        updateScore(score);
+        
+        cross = false;
+        setTimeout(function () {
+            cross = true;
+        }, 100);
+    }
+}, 10);
+function updateScore(score) {
+    var scoreCont = document.getElementById("scoreCont");
+    scoreCont.innerHTML = "Your Score:" + "" + score;
+}
+
+
